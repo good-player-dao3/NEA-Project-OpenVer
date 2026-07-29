77f483319483d182e3a532747952c1870024bf23
CodeManTeam <weareqytx@gmail.com>
2026-07-29 17:46:09 +0800

    chore: stop tracking local Lokibox files

--- Lokibox/lokibox-merged-bypass/desktop/profile-preload.cjs
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('dao3Profiles', {
-  list: () => ipcRenderer.invoke('profiles:list'),
-  create: profile => ipcRenderer.invoke('profiles:create', profile),
-  remove: id => ipcRenderer.invoke('profiles:delete', id),
-  activate: id => ipcRenderer.invoke('profiles:activate', id),
-  deactivate: () => ipcRenderer.invoke('profiles:deactivate'),
-});
